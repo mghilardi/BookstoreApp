b09bb64d12c80c01d37528c09df6f180c6518591
MDG <mauricio.ghilardi@gmail.com>
2024-10-02 18:41:00 +0200

    Add contact component

---
+++ src/pages/contact/Contact.jsx
@@ -0,0 +1,23 @@
+const Contact = () => {
+  return (
+    <>
+      <div className="mt-4 mx-5 text-center">
+        <i className="mb-2 bi bi-book-half " style={{ fontSize: "2em" }} />
+        <p>
+          <i className="bi bi-person-circle" /> Ludwig van Beethoven
+        </p>
+        <p>
+          <i className="bi bi-geo" /> Bonngasse 22-24, 53111 Bonn, Germany
+        </p>
+        <p>
+          <i className="bi bi-envelope" /> ludwig@beethoven.de
+        </p>
+        <p>
+          <i className="bi bi-telephone" /> +49 228 9817525
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
